9ee9ca918c6a7cc80547285921598dbaee5852ba
Raees <raees.s@reapmind.com>
2026-04-25 22:15:43 +0530

    firebase setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:pro_mini/view/chat_screen.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();  
-  await dotenv.load(fileName: ".env"); 
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await dotenv.load(fileName: ".env");
   runApp(const ProviderScope(child: MyApp()));
 }
 
